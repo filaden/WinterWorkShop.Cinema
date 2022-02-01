882a9bc5f7a3f4cad0fe6a061406d68432ced031
Anela Moćević <anelamocevic@gmail.com>
2022-02-01 05:18:12 -0800

    adding properties

--- WinterWorkShop.Cinema.Data/Models/MovieModel.cs
+++ WinterWorkShop.Cinema.Data/Models/MovieModel.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public double Rating { get; set; }
     }
 }
